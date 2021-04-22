f57b590baa2b1e1e08995232337ad0a3db8d99ee
Bara Abdullatif <bara.abd@yahoo.com>
2021-04-22 14:30:44 +0200

    Use state Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,28 @@
-import react from "react";
+import react, { useState } from "react";
 import "./App.scss";
 import Input from "./components/input";
 import NameTag from "./components/nameTag";
 
-
-const makeGreen = BaseComponent => props => {
-  const addGreen = {
-    style: {
-      color: "lightgreen"
-    }
-  }
-  const newProps = {
-    ...props, ...addGreen
-  }
-
-  return <BaseComponent {...newProps}/>
-}
-
-const removeInline = BaseComponent => props => {
-  const newProps = {...props}
-  delete newProps.style
-
-  return <BaseComponent {...newProps}/>
-}
-
-const GreenNewTag = makeGreen(NameTag)
-const CleanNameTag = removeInline(NameTag)
-
-
 function App() {
+  const [age, setAge] = useState(21);
+
+  const ageUpHandle = () => {
+    setAge(age +1)
+  }
+
+  const ageDownHandle = () => {
+    setAge(age -1)
+  }
+
   return (
     <div>
       <header className="App-header">
-        <h1 className="nameStyle nameTagTitle">Names List</h1>
-        {/*<Input placeholder="Skrev ditt namn här!" type="text"></Input> */}
-        <GreenNewTag firstName="John" lastName="Anderson"></GreenNewTag>
-        <CleanNameTag style={{color:'red'}} firstName="Joli" lastName="Johanson"></CleanNameTag>
-        <NameTag style={{color:'red'}} firstName="Hanna" lastName="Peterson"></NameTag>
-        <NameTag></NameTag>
-
+        <h1>Use State Hooks</h1>
+        <h2>Age: {age} </h2>
+        <div className="buttonContainer">
+          <button onClick={ageUpHandle} className="buttonStyle">Age up</button>
+          <button onClick={ageDownHandle} className="buttonStyle">Age down</button>
+        </div>
       </header>
     </div>
   );
